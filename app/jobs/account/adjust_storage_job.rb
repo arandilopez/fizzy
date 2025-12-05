6231494c79498c03e76421a3b896451884c43901
Jorge Manrubia <jorge@hey.com>
2025-12-05 12:30:36 +0100

    Sequentalize these jobs on a per-account basis
    To keep the write load under control when enqueuing a ton of these

--- app/jobs/account/adjust_storage_job.rb
+++ app/jobs/account/adjust_storage_job.rb
@@ -1,4 +1,6 @@
 class Account::AdjustStorageJob < ApplicationJob
+  limits_concurrency to: 1, key: ->(account, delta) { account }
+
   def perform(account, delta)
     account.adjust_storage(delta)
   end
